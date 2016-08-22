ca856417a9dc3606d6dec624825d240d362f1d7f
Blake Griffith <blake.a.griffith@gmail.com>
2016-08-22 15:41:05 -0700

    Integrate contacts.json

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, jsonify, request
+import os
+from flask import render_template, jsonify, request, json
 
 from app import app
 
@@ -8,27 +9,35 @@ def index():
 
 @app.route('/department/<dept>')
 def department(dept):
-    return render_template('department.html', department=dept)
+    return render_template('department.html', department=departments[dept])
 
 @app.route('/search', methods=['POST'])
 def search():
     query = request.json['query']
     res = list(map(lambda x: x.__repr__(),
-                   filter(lambda d: d.search(query), departments)))
+                   filter(lambda d: d.search(query), department_list)))
     return jsonify(res)
 
 class Department:
-    def __init__(self, name):
+    def __init__(self, name, data):
         # TODO
         #, description, contact)
         self.name = name
         self.stemmed_name = stemmer(name)
-        """TODO
-        self.description = description
-        self.contact = contact
-        self.stemmed_description = stemmer(description)
-        self.stemmed_contact = 
-        """
+
+        self.email = data['email']
+        self.contact_name = data['name']
+        self.notes = data['notes']
+        self.url = data['url']
+
+        self.stemmed_email = stemmer(self.email)
+        self.stemmed_contact_name = stemmer(self.contact_name)
+        self.stemmed_notes = stemmer(self.notes)
+
+    @property
+    def localurl(self):
+        return "/department/%s" % self.name
+
     def search(self, query):
         query = stemmer(query)
         if query in self.stemmed_name:
@@ -46,4 +55,12 @@ class Department:
 def stemmer(string):
     return string.lower()
 
-departments = [Department(d) for d in ['water', 'trash', 'cops']]
+def get_contacts():
+    root = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(root, 'static', 'contacts.json')
+    with open(json_url) as f:
+        return json.load(f)['contacts']['San Francisco']
+
+contacts = get_contacts()
+departments = dict((k, Department(k, v)) for k, v in contacts.items())
+department_list = departments.values()
