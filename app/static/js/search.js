92b7d298a155969688ef7e5f84bae8dbbf9807a8
Blake Griffith <blake.a.griffith@gmail.com>
2016-08-22 12:08:23 -0700

    Add incremental search.

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -2,7 +2,18 @@ function onSearch(e) {
   var query = document.getElementById("search-box").value;
 
   search(query);
+}
 
+function parseSearch(results) {
+  var out = "<ul>";
+  var rlen = results.length;
+
+  for (var i = 0; i < rlen; i++) {
+      var dept = results[i];
+      out += "<li><a href=\"/department/" + dept + "\">" + dept + "</a></li>";
+  }
+  out += "</ul>";
+  return out;
 }
 
 function search(query) {
@@ -13,7 +24,7 @@ function search(query) {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var json = JSON.parse(xhr.responseText);
       var resultEl = document.getElementById("search-results");
-      resultEl.innerText = query;
+      resultEl.innerHTML = parseSearch(json);
     }
   }
   var data = JSON.stringify({"query": query});
